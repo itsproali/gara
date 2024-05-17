4969d9b01ba609d2deada65d22cf7fc75bc4a4d7
Mohammad Ali <itsproali@gmail.com>
2024-05-17 12:05:24 +0600

    media page added

--- src/app/pages/media/media.component.ts
+++ src/app/pages/media/media.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NewsCardComponent } from '../../components/news-card/news-card.component';
+import { INews } from '../../core/models/interfaces/INews';
+import { allNews } from '../../core/constant/allNews';
 
 @Component({
   selector: 'app-media',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, NewsCardComponent],
   templateUrl: './media.component.html',
-  styleUrl: './media.component.css'
+  styleUrl: './media.component.css',
 })
 export class MediaComponent {
-
+  allNews: INews[] = allNews.slice(0, 3);
 }
